fe5ba1d87a56c856a140082106066a1f094cd8e1
MohamedyasinM <mohamedyasin.m17@gmail.com>
2025-05-10 14:48:26 +0530

    Updated Code

--- Docling.py
+++ Docling.py
@@ -1,69 +1,54 @@
-from docling.document_converter import DocumentConverter , PdfFormatOption
-from docling.datamodel.pipeline_options import PdfPipelineOptions
-from docling.datamodel.base_models import InputFormat
-# # from utils.sitemap import get_sitemap_urls
+from docling.datamodel.base_models import FigureElement, InputFormat, Table
+from docling.backend.docling_parse_backend import DoclingParseDocumentBackend
+from docling. datamodel.base_models import InputFormat
+from docling.document_converter import DocumentConverter, PdfFormatOption
+from docling_core. types.doc import ImageRefMode, PictureItem, TableItem
+from docling.datamodel.pipeline_options import (
+    EasyOcrOptions,
+    OcrMacOptions,
+    PdfPipelineOptions,
+    RapidOcrOptions,
+    TesseractCliOcrOptions,
+    TesseractOcrOptions,
+)
+import time
+import requests
+from pathlib import Path
+from IPython.display import display
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+import os
 
+IMAGE_RESOLUTION_SCALE = 20
+input_doc_path = Path("./sample.pdf")
 
-
+# Set pipeline options
 pipeline_options = PdfPipelineOptions()
-pipeline_options.generate_picture_images = True
+pipeline_options.images_scale = IMAGE_RESOLUTION_SCALE
 pipeline_options.generate_page_images = True
-pipeline_options.images_scale = 2
-pipeline_options.do_picture_classification = True
+pipeline_options.generate_picture_images = True
 
-converter = DocumentConverter(format_options={
-    InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)
-})
+# Create document converter with format options
+doc_converter = DocumentConverter(
+    format_options={
+        InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)
+    }
+)
 
-result = converter.convert("./sample.pdf")
-# result = converter.convert("./Test.pdf")
-document = result.document
-markdown_output = document.export_to_markdown()
-json_output = document.export_to_dict()
-print(markdown_output)
+# Perform conversion and measure time
+start_time = time.time()
+conv_res = doc_converter.convert(input_doc_path)
+end_time = time.time() - start_time
 
-with open("output.md", "w", encoding="utf-8") as f:
-    f.write(document.export_to_markdown())
+print(f"Total time taken for parsing: {end_time:.2f} seconds.")
+print("Document Name:", conv_res.document.origin.filename)
+print("Document Type:", conv_res.document.origin.mimetype)
+print("Number of Pages:", len(conv_res.document.pages))
+print("Number of Text Blocks:", len(conv_res.document.texts))
+print("Number of Tables:", len(conv_res.document.tables))
+print("Number of Images:", len(conv_res.document.pictures))
+print(conv_res.document.export_to_markdown())
 
-# import os
-# os.environ['HF_HOME'] = os.path.abspath('./hf_cache')  # Or any safe directory
-# os.environ['HF_HUB_DISABLE_SYMLINKS_WARNING'] = '1'
-# os.makedirs("figures", exist_ok=True)
-# import streamlit as st
-#
-# # Example usage
-# # converter = DocumentConverter()
-# result = converter.convert("D:/docling/utils/testing_doc2.pdf")
-#
-# document = result.document
-# markdown_output = document.export_to_markdown()
-# json_output = document.export_to_dict()
-#
-# # Show the markdown on frontend
-# st.markdown(markdown_output, unsafe_allow_html=True)  # if HTML tags are inside markdown
-#
-# # Optionally show JSON too
-# st.json(json_output)
 
-# --------------------------------------------------------------
-# Basic HTML extraction
-# --------------------------------------------------------------
 
-# result = converter.convert("https://docling-project.github.io/docling/concepts/docling_document/")
-#
-# document = result.document
-# markdown_output = document.export_to_markdown()
-# print(markdown_output)
-#
-# # --------------------------------------------------------------
-# # Scrape multiple pages using the sitemap
-# # --------------------------------------------------------------
-#
-# sitemap_urls = get_sitemap_urls("https://docling-project.github.io/docling/concepts/docling_document/")
-# conv_results_iter = converter.convert_all(sitemap_urls)
-#
-# docs = []
-# for result in conv_results_iter:
-#     if result.document:
-#         document = result.document
-#         docs.append(document)
